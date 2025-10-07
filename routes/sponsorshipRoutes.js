33c3f90e6fa8d37c37623a12d6b3a81f99a7020e
PeterHesse74 <peterhesse74@gmail.com>
2025-10-07 14:07:05 +0500

    remove token in the sponser api

--- routes/sponsorshipRoutes.js
+++ routes/sponsorshipRoutes.js
@@ -13,8 +13,8 @@ import auth_token from "../middleware/auth_token.js";
 
 const router = Router();
 
-router.get("/", auth_key_header, auth_token, listSponsorships);
-router.get("/all", auth_key_header, auth_token, allListSponsorships);
+router.get("/", auth_key_header, listSponsorships);
+router.get("/all", auth_key_header, allListSponsorships);
 router.get("/:id", auth_key_header, auth_token, getSponsorshipById);
 router.post("/", auth_key_header, auth_token, createSponsorship);
 router.patch("/:id", auth_key_header, auth_token, updateSponsorship);
